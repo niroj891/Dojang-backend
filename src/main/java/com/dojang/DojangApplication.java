f9160c07c5dde12b19f1d9cff91991b18cb2bbf0
niroj891 <nirojpanta96@gmail.com>
2024-12-12 11:43:27 +0545

    Initial file

---
+++ src/main/java/com/dojang/DojangApplication.java
@@ -0,0 +1,13 @@
+package com.dojang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dojang/DojangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
